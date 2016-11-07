bff12c188be7994d3650955914a62e934deb4518
Jiar <jiar.world@gmail.com>
2016-11-07 22:51:39 +0800

    init

---
+++ Application/Shopping/Controller/ShoppingController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by: Jiar
+ * Github: https://www.github.com/Jiar/
+ */
+
+namespace Shopping\Controller;
+
+use Think\Controller;
+
+class ShoppingController extends Controller {
+
+    public function shopping_action() {
+        echo "<h1>hello</h1>>";
+    }
+
+}
